a77b2193116bd0f3fe7fb3d3f6cd6bd7645e6843
Henrik Fuchs <atheck@gmail.com>
2022-04-11 08:28:47 +0200

    fix: disable rule unicorn/no-useless-switch-case for now
    Reason: Definition for rule 'unicorn/no-useless-switch-case:' was not found. Is it a bug?

--- node.js
+++ node.js
@@ -732,7 +732,8 @@ const rules = {
     "unicorn/no-useless-length-check": "error",
     "unicorn/no-useless-promise-resolve-reject": "error",
     "unicorn/no-useless-spread": "error",
-    "unicorn/no-useless-switch-case:": "error",
+    // TODO: Definition for rule 'unicorn/no-useless-switch-case:' was not found
+    // "unicorn/no-useless-switch-case:": "error",
     "unicorn/no-useless-undefined": "error",
     "unicorn/no-zero-fractions": "error",
     "unicorn/number-literal-case": "error",
