1d761707a3b5220afd5d0e8dd575f405742bcc40
Henrik Fuchs <atheck@gmail.com>
2025-03-23 13:31:20 +0100

    fix: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
+import { defineConfig } from "eslint/config";
 import complexity from "./complexity.js";
 import groupImports from "./groupImports.js";
 import nodeWithBiome from "./nodeWithBiome.js";
 
 // biome-ignore lint/style/noDefaultExport: Required for ESLint configuration
-export default [...nodeWithBiome, ...complexity, ...groupImports];
+export default defineConfig({
+	extends: [nodeWithBiome, complexity, groupImports],
+});
