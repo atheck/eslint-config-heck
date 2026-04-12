272601cb0222728aa0b558bb74c81c605dcf4ec9
Henrik Fuchs <atheck@gmail.com>
2026-04-12 13:30:10 +0200

    fix: use typescript-eslint's rule for return types again
    It is more powerful than biome's one.

--- nodeWithBiome.js
+++ nodeWithBiome.js
@@ -760,7 +760,16 @@ export default [
 			"@typescript-eslint/default-param-last": "off",
 			"dot-notation": "off",
 			"@typescript-eslint/dot-notation": "off",
-			"@typescript-eslint/explicit-function-return-type": "off",
+			"@typescript-eslint/explicit-function-return-type": [
+				"error",
+				{
+					allowExpressions: true,
+					allowTypedFunctionExpressions: true,
+					allowHigherOrderFunctions: true,
+					allowDirectConstAssertionInArrowFunctions: true,
+					allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+				},
+			],
 			"@typescript-eslint/explicit-member-accessibility": "off",
 			"@typescript-eslint/explicit-module-boundary-types": "off",
 			"init-declarations": "off",
