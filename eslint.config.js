34ef63e730b95f48b141610bf3d25d8cf48d3cdb
Henrik Fuchs <atheck@gmail.com>
2026-04-24 17:07:22 +0200

    chore: update linting configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "eslint/config";
-import complexity from "./complexity.js";
-import groupImports from "./groupImports.js";
 import nodeWithBiome from "./nodeWithBiome.js";
 
 // biome-ignore lint/style/noDefaultExport: Required for ESLint configuration
 export default defineConfig({
-	extends: [nodeWithBiome, complexity, groupImports],
+	extends: [nodeWithBiome],
 });
