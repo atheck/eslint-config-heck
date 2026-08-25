3ac90a82b8ed1a02b9217448a142d129d1e94d65
Henrik Fuchs <atheck@gmail.com>
2026-08-25 08:37:30 +0200

    fix: update deps

--- nodeWithBiome.js
+++ nodeWithBiome.js
@@ -956,7 +956,7 @@ export default [
 			"@typescript-eslint/no-unsafe-function-type": "off",
 			"@typescript-eslint/no-unsafe-member-access": "error",
 			"@typescript-eslint/no-unsafe-return": "error",
-			"@typescript-eslint/no-unsafe-type-assertion": "error",
+			"@typescript-eslint/no-unsafe-type-assertion": "off",
 			"@typescript-eslint/no-unsafe-unary-minus": "error",
 			"no-unused-expressions": "off",
 			"@typescript-eslint/no-unused-expressions": "off",
